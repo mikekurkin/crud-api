8b2d5d47d5e60706ec0b00e3bbd23060823f680b
Mike Kurkin <mike@kurkin.net>
2024-02-13 00:29:13 +0300

    test: add tests

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,34 @@ import { createServer as createServerHttp } from 'node:http';
 import { parse } from 'querystring';
 import { route } from './route';
 
-const app = () => {
-  const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-  const crudServer = createServerHttp((req, res) => {
-    let body = '';
-    const requestData = { post: {}, get: {} };
+export const server = createServerHttp((req, res) => {
+  let body = '';
+  const requestData = { post: {}, get: {} };
 
-    const handler = route(req.method, req.url);
+  const handler = route(req.method, req.url);
 
-    if (req.url)
-      requestData.get = new URL(
-        req.url,
-        `http://${req.headers.host}`,
-      ).searchParams;
+  if (req.url)
+    requestData.get = new URL(
+      req.url,
+      `http://${req.headers.host}`,
+    ).searchParams;
 
-    req.on('data', (chunk) => (body += chunk));
-    req.on('end', () => {
-      requestData.post = parse(body);
+  req.on('data', (chunk) => (body += chunk));
+  req.on('end', () => {
+    console.log(body);
+    requestData.post = parse(body);
 
-      const { code, data } = handler(requestData);
+    const { code, data } = handler(requestData);
 
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(code);
-      res.end(JSON.stringify(data));
-    });
+    res.setHeader('Content-Type', 'application/json');
+    res.writeHead(code);
+    res.end(JSON.stringify(data));
   });
+});
 
-  crudServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
-  });
-};
-
-app();
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('To terminate it, use Ctrl+C combination');
+});
